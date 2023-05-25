ddcf1c6953608b478873275a9f99aa4b822d5207
TritiumR <101703696+TritiumR@users.noreply.github.com>
2023-05-25 19:31:31 +0800

    sh

--- tools/run_test_few_shot_one.sh
+++ tools/run_test_few_shot_one.sh
@@ -1,7 +1,7 @@
-categories="Microwave"
+categories="Faucet Window"
 for category in $categories
 do
-  bash ./scripts/train_BERT.sh 0 \
+  bash ./scripts/train_BERT.sh 1 \
     --config cfgs/ModelNet_models/PointTransformer_8192point.yaml \
     --test \
     --ckpts ./Point-BERT.pth \
@@ -9,9 +9,9 @@ do
     --num_interaction_data_offline 100 \
     --batch_size 16 \
     --num_point_per_shape 8192 \
-    --offline_data_dir ./confidence_data/gt_data-train_fixed_cam_new_new_test_data-pulling \
+    --offline_data_dir ./confidence_data/gt_data-train_fixed_cam_new_new_test_data-pushing \
     --category_types "$category" \
-    --primact_type pulling \
+    --primact_type pushing \
     --buffer_max_num 200000 \
     --start_epoch 0 \
     --no_true_false_equal \
--- tools/run_train_few_shot_one_ranking.sh
+++ tools/run_train_few_shot_one_ranking.sh
@@ -1,20 +1,20 @@
-categories="Switch Kettle"
+categories="Faucet Window"
 for category in $categories
 do
   bash ./scripts/train_BERT.sh 1 \
     --config cfgs/ModelNet_models/PointTransformer_8192point.yaml \
     --finetune_model \
     --ckpts ./Point-BERT.pth \
-    --exp_name few-shot_"$category" \
+    --exp_name few-shot_"$category"_pulling \
     --num_interaction_data_offline 6 \
     --batch_size 16 \
     --num_point_per_shape 8192 \
-    --offline_data_dir ./confidence_data/gt_data-train_fixed_cam_new_new_ruler_data-pushing \
-    --additional_data_dir ./confidence_data/gt_data-train_fixed_cam_train_data-pushing \
+    --offline_data_dir ./confidence_data/gt_data-train_fixed_cam_new_new_ruler_data-pulling \
+    --additional_data_dir ./confidence_data/gt_data-train_fixed_cam_train_data-pulling \
     --additional_category_types StorageFurniture \
     --additional_num_interaction 1 \
     --category_types "$category" \
-    --primact_type pushing \
+    --primact_type pulling \
     --buffer_max_num 100000 \
     --epoch 20 \
     --sample_max_num 10 \
